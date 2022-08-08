99d14291e085afde27fe3d87d392d3aa5d66ccca
ThEVlaDGamE <thevlad2005@yandex.ru>
2022-08-08 22:45:17 +0300

    Первый коммит

---
+++ SongsAverageDuration2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using TagLib;
+
+namespace Application
+{
+    class Program
+    {
+        static string[] fullfilesPath = Directory.GetFiles("songs/", "*.mp3*", SearchOption.AllDirectories);
+        static List<double> songsDuration = new List<double>();
+
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < fullfilesPath.Length; i++)
+            {
+                var file = TagLib.File.Create(fullfilesPath[i]);
+                Console.WriteLine(fullfilesPath[i] + "  ---  " +  file.Properties.Duration.TotalSeconds.ToString());
+
+                songsDuration.Add(file.Properties.Duration.TotalSeconds);
+            }
+
+            Console.WriteLine("\n\n");
+            double summ = 0;
+            for (int i = 0; i < songsDuration.Count; i++)
+            {
+                summ += songsDuration[i];
+            }
+
+            int result = (int)Math.Round(summ / songsDuration.Count);
+            Console.WriteLine(result);
+
+            int resultMin = 0;
+            while(result >= 60)
+            {
+                resultMin++;
+                result -= 60;
+            }
+            Console.WriteLine(resultMin + " минут " + result + " секунд");
+        }
+    }
+}
+
+
+
+
